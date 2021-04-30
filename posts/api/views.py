272eeb0b0eece03a63f010c1d5c5a31d1c2c74b8
mmAbdelhay <abdelhay.courses@gmail.com>
2021-04-30 23:56:30 +0200

    adding CRUD operations for posts using token & url will be "http://127.0.0.1:8000/api/posts/(operations)"

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,70 +1,61 @@
-from rest_framework.response import Response
 from rest_framework import status
-from posts.models import Post
-from .serializers import PostSerializer, UserSerializer
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, BasePermission
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-
-class IsViewer(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.group.filter(name='viwers').exists()
+from Users.models import Post
+from posts.api.serializers import PostSerializer
 
 
 @api_view(["GET"])
-@permission_classes([IsAuthenticated, IsViewer])
+@permission_classes([IsAuthenticated])
 def index(request):
     posts = Post.objects.all()
     serializer = PostSerializer(instance=posts, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+
 @api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def create(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(data={
             "success": True,
-            "message": "post has been created successfully"
-        },status=status.HTTP_201_CREATED)
-    
+            "message": "post has been added successfully"
+        }, status=status.HTTP_201_CREATED)
+
     return Response(data={
         "success": False,
         "errors": serializer.errors
-    },status=status.HTTP_400_BAD_REQUEST)
+    }, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["POST"])
-def api_signup(request):
-    serializer = UserSerializer(data=request.data)
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete(request, id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(instance=post, many=False)
+    serializer.delete()
+    return Response(data={
+        'message': 'deleted',
+        'success': True
+    }, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update(request, id):
+    post = Post.objects.get(pk=id)
+    serializer = PostSerializer(instance=post, many=False, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(data={
-            "success": True,
-            "message": "post has been registerd succesfully"
-        },status=status.HTTP_201_CREATED)
-    
+            'message': 'post updated',
+            'success': True
+        }, status=status.HTTP_201_CREATED)
     return Response(data={
-        "success": False,
-        "errors": serializer.errors
-    },status=status.HTTP_400_BAD_REQUEST)
-
-
-class CreatePost(generics.CreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class ListPost(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-class CrudPosts(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+        'Error': serializer.errors,
+        'success': False
+    }, status=status.HTTP_400_BAD_REQUEST)
--- posts/forms.py
+++ posts/forms.py
@@ -1,27 +1,17 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Post
+from Users.models import Post
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = "__all__"
-        exclude = ("metrics",)
-
-    def clean_title(self):
-        title = self.cleaned_data.get("title")
-        if "test" in title:
-            raise ValidationError("title shouldn't has test word!")
-
-        return title
 
     def clean(self):
-        # hna a2dr aktb validation bra7ty 3la kol 7aga
         super(PostForm, self).clean()
         content = self.cleaned_data.get("content")
-        title = self.cleaned_data.get("title")
-        if len(content) < 10:
-            raise ValidationError("content must be bigger than 10 chars")
+        if len(content) < 5:
+            raise ValidationError("content must be bigger than 5 chars")
 
         return self.cleaned_data
